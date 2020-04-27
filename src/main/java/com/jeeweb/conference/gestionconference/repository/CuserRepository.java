adb05ad6978723af4e848f0abc99e402bcf01b31
rain man <45740145+ayoubmoussaid@users.noreply.github.com>
2020-04-27 15:43:37 +0000

    database connected succesfully

--- src/main/java/com/jeeweb/conference/gestionconference/repository/CuserRepository.java
+++ src/main/java/com/jeeweb/conference/gestionconference/repository/CuserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface CuserRepository extends MongoRepository<Cuser, Integer> {
-
 	Cuser findCuserById(int id);
 }
