849fbaa519e535a15438528cdd5a077231dd491f
Achraf Lasri <48652079+hamidelmerdy@users.noreply.github.com>
2020-04-27 14:32:13 +0000

    man3rf

--- src/main/java/com/jeeweb/conference/gestionconference/config/MongoDBConfig.java
+++ src/main/java/com/jeeweb/conference/gestionconference/config/MongoDBConfig.java
@@ -2,6 +2,8 @@ package com.jeeweb.conference.gestionconference.config;
 
 import com.jeeweb.conference.gestionconference.documents.Session;
 import com.jeeweb.conference.gestionconference.documents.User;
+import com.jeeweb.conference.gestionconference.repository.PropositionRepository;
+import com.jeeweb.conference.gestionconference.repository.ReviewsRepository;
 import com.jeeweb.conference.gestionconference.repository.SessionRepository;
 import com.jeeweb.conference.gestionconference.repository.UsersRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -17,10 +19,21 @@ import java.util.stream.Stream;
 @Configuration
 public class MongoDBConfig {
 	@Bean
-	CommandLineRunner commandLineRunner(UsersRepository usersRepository, SessionRepository sessionRepository) {
+	CommandLineRunner commandLineRunner(UsersRepository usersRepository,
+										SessionRepository sessionRepository,
+										PropositionRepository propositionRepository,
+										ReviewsRepository reviewsRepository
+										) {
 		return onfeargs -> {
 			usersRepository.save(new User(1, "ayoub", "moussaid", 1500L));
 			usersRepository.save(new User(2, "youssef", "hamid", 2300L));
+
+			sessionRepository.deleteAll();
+			Stream.of("S1 Session une", "S2 Session tanya", "S3 Session talta").forEach(s -> {
+				sessionRepository.save(new Session(s.split(" ")[0], s.split(" ")[1], new Date(), "Room"+s.split(" ")[0]));
+			});
+			sessionRepository.findAll().forEach(System.out::println);
+
 			sessionRepository.deleteAll();
 			Stream.of("S1 Session une", "S2 Session tanya", "S3 Session talta").forEach(s -> {
 				sessionRepository.save(new Session(s.split(" ")[0], s.split(" ")[1], new Date(), "Room"+s.split(" ")[0]));
